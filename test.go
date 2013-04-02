487aba04b827673f49f6a036765db60db85d3295
Matthias Bauer <moeffju@moeffju.net>
2013-04-02 14:54:49 +0200

    gofmt

--- test.go
+++ test.go
@@ -1,23 +1,25 @@
 package main
 
-import irc "github.com/fluffle/goirc/client"
-import "fmt"
+import (
+	"fmt"
+	irc "github.com/fluffle/goirc/client"
+)
 
 func main() {
-    c := irc.SimpleClient("goilertest")
-    c.SSL = true
+	c := irc.SimpleClient("goilertest", "goiler", "goiler_in ist goiler als noiler")
+	c.SSL = true
+	c.EnableStateTracking()
 
-    // join channel on connect
-    c.AddHandler(irc.CONNECTED, func(conn *irc.Conn, line *irc.Line) { conn.Join("#nodrama.de") })
-    // emit a signal on disconnect
-    quit := make(chan bool)
-    c.AddHandler(irc.DISCONNECTED, func(conn *irc.Conn, line *irc.Line) { quit <- true })
+	// join channel on connect
+	c.AddHandler(irc.CONNECTED, func(conn *irc.Conn, line *irc.Line) { conn.Join("#furanzentest") })
+	// emit a signal on disconnect
+	quit := make(chan bool)
+	c.AddHandler(irc.DISCONNECTED, func(conn *irc.Conn, line *irc.Line) { quit <- true })
 
-    err := c.Connect("irc.freenode.net")
-    if err != nil {
-        fmt.Printf("Connection error: %s\n", err)
-    }
+	if err := c.Connect("irc.freenode.net"); err != nil {
+		fmt.Printf("Connection error: %s\n", err)
+	}
 
-    // Wait for disconnect
-    <-quit
+	// Wait for disconnect
+	<-quit
 }
